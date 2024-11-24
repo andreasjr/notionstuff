d6c17d7f4bc2c1f67f4b67272b3833ed658cebcb
Andreas Reif <andreas@sitecare.com>
2024-11-23 16:42:06 -0800

    feat: add gitignore feat: add styles for my homepage

---
+++ andreasjr-home.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', () => {
+	// Select the node that will be observed for mutations
+	const targetNode = document.getElementById("notion-app");
+
+	// Options for the observer (which mutations to observe)
+	const config = { attributes: true, childList: true, subtree: true };
+
+	// Callback function to execute when mutations are observed
+	const callback = (mutationList, observer) => {
+	  let notionButton = document.querySelector('.notion-close-sidebar[style*="opacity: 1"]')?.click();
+	};
+
+	// Create an observer instance linked to the callback function
+	const observer = new MutationObserver(callback);
+
+	// Start observing the target node for configured mutations
+	observer.observe(targetNode, config);
+
+	createFooter();
+	
+});
+
+
+function createFooter() {
+	document.getElementById('andreasjr-footer').innerHTML = ``;
+	// <div class="content">
+	// <img src="/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F2a4f1a11-0ebe-4a3a-84f0-c5477f6aa6af%2F1f6732d8-7383-4494-8010-bf3ff9336160%2F3fbe56a9-ac9c-47e1-a05d-614bb38696b8.png?id=f6a87184-dc71-4f41-afc4-2f66df49e92c&table=block&spaceId=2a4f1a11-0ebe-4a3a-84f0-c5477f6aa6af&favicon=true&userId=&cache=v2" /><br/>
+	// <h3>Andreas Reif</h3>
+	// </div>
+}
